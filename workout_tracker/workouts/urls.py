783eff21ba3315cb197f23333d93025a98bc345c
Adam Myers <adammyers002@gmail.com>
2026-02-26 13:48:12 -0600

    switch home route to TemplateView and prep workouts views

--- workout_tracker/workouts/urls.py
+++ workout_tracker/workouts/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # auth
     path("", views.Home.as_view(), name="home"),
+    # workouts
     path("accounts/signup/", views.SignUp.as_view(), name="signup"),
     path("workouts/", views.WorkoutList.as_view(), name="workouts_index"),
 ]
--- workout_tracker/workouts/views.py
+++ workout_tracker/workouts/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+
+from .models import Workout, ExerciseEntry, Tag
+from .forms import ExerciseEntryForm, SignUpForm
 
 
-def home(request):
-    return HttpResponse("Workout Tracker")
+class Home(TemplateView):
+    template_name = "home.html"
